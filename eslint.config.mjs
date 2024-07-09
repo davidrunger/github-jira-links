80b4dfeed1e0570f001908266af702c24d1983f5
David Runger <davidjrunger@gmail.com>
2024-07-09 01:00:25 -0500

    Migrate to ESLint 9 config
    `npx @eslint/migrate-config .eslintrc.js`

---
+++ eslint.config.mjs
@@ -0,0 +1,87 @@
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends("eslint:recommended"), {
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            chrome: false,
+        },
+
+        ecmaVersion: "latest",
+        sourceType: "module",
+
+        parserOptions: {
+            parser: "@typescript-eslint/parser",
+        },
+    },
+
+    settings: {
+        "import/parsers": {
+            "@typescript-eslint/parser": [".ts"],
+        },
+
+        "import/resolver": {
+            typescript: {
+                alwaysTryTypes: true,
+            },
+        },
+    },
+
+    rules: {
+        "@typescript-eslint/no-empty-function": "off",
+        "arrow-parens": "off",
+        "function-paren-newline": "off",
+        "import/no-extraneous-dependencies": "off",
+        "import/prefer-default-export": "off",
+
+        "max-len": ["warn", {
+            code: 100,
+            ignoreUrls: true,
+        }],
+
+        "newline-per-chained-call": "off",
+        "no-alert": "off",
+        "no-console": "warn",
+        "no-debugger": "warn",
+        "no-else-return": "off",
+        "no-new": "off",
+        "no-param-reassign": "off",
+        "no-plusplus": "off",
+
+        "no-restricted-imports": ["error", {
+            paths: [{
+                name: "lodash",
+                message: "Use lodash-es.",
+            }],
+        }],
+
+        "no-restricted-syntax": ["error", "ForInStatement", "LabeledStatement", "WithStatement"],
+        "no-underscore-dangle": "off",
+        "no-unreachable": "warn",
+
+        "no-use-before-define": ["error", {
+            functions: false,
+        }],
+
+        "no-unused-vars": "off",
+        "operator-linebreak": "off",
+
+        quotes: ["warn", "single", {
+            avoidEscape: true,
+        }],
+
+        "require-await": "error",
+    },
+}];
